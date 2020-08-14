a5daded559ca8616ca6e4316a384fd0f3358832a
1392122452 <56390317+1392122452@users.noreply.github.com>
2020-08-14 14:06:10 +0800

    Add files via upload

---
+++ StudentManagement/ViewModel/HomeDataViewModel.cs
@@ -0,0 +1,15 @@
+﻿using StudentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.ViewModel
+{
+    public class HomeDataViewModel
+    {
+        public Student Student { get; set; }
+        public string PageTitle { get; set; }
+
+    }
+}
---
+++ StudentManagement/ViewModel/StudentsAddSViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using StudentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.ViewModel
+{
+    public class StudentsAddSViewModel
+    {
+        
+
+        [Display(Name = "姓名")]
+
+        [Required(ErrorMessage = "请输入名字"), MaxLength(50, ErrorMessage = "名字长度不能超过50个字符")]
+        public string Name { get; set; }
+
+        [Display(Name = "年级")]
+        [Required(ErrorMessage = "请输入年级")]
+        public ClassNameEnum? ClassName { get; set; }
+
+        [Display(Name = "邮箱")]
+        [Required(ErrorMessage = "请输入邮箱")]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            ErrorMessage = "邮箱的格式不正确")]   //邮箱验证的正则表达式
+        public string Email { get; set; }
+
+        [Display(Name="头像")]
+        public List<IFormFile> Photos { get; set; }
+
+    }
+}
