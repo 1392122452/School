456bd641ee3369c2439ac08779f57340f1ba37ac
1392122452 <56390317+1392122452@users.noreply.github.com>
2020-08-14 13:57:09 +0800

    Add files via upload
    数据迁移文件

---
+++ StudentManagement/Migrations/20200716094953_SeedStudentsTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentManagement.Migrations
+{
+    public partial class SeedStudentsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    ClassName = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "ClassName", "Email", "Name" },
+                values: new object[] { 1, 2, "1392122452@qq.com", "耿家楷" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ StudentManagement/Migrations/20200716100444_AlterStudentsTableData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentManagement.Migrations
+{
+    public partial class AlterStudentsTableData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "ClassName", "Email", "Name" },
+                values: new object[] { 2, 2, "1014283424@qq.com", "耿加林" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
